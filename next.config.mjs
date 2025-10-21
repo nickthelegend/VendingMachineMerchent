d7ffa400ade6b99cceaeb37fad5ddfa05a349c00
nickthelegend <testingtesla7@gmail.com>
2025-10-21 17:57:53 +0530

    Prisma Build when npx buildd

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
 }
 
 export default nextConfig
